8521cebf2a0f94a3a29785290ce12077eecf923b
s25509 <s25509@pjwstk.edu.pl>
2024-04-15 21:31:59 +0200

    updated AnimalDTO

--- RestApi/Animals/AnimalDTO.cs
+++ RestApi/Animals/AnimalDTO.cs
@@ -16,3 +16,15 @@ public class CreateAnimalDTO
     [MaxLength(200)]
     public string Area { get; set; }
 }
+
+public class UpdateAnimalDTO
+{
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+    [MaxLength(200)]
+    public string Category { get; set; }
+    [MaxLength(200)]
+    public string Area { get; set; }
+}
