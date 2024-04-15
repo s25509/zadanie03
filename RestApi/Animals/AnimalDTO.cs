8ca1f16a94f91c6465a2f1749e881011cb1e3684
s25509 <s25509@pjwstk.edu.pl>
2024-04-15 21:31:08 +0200

    added AnimalDTO

---
+++ RestApi/Animals/AnimalDTO.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestApi.Animals;
+
+public class CreateAnimalDTO
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Category { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Area { get; set; }
+}
